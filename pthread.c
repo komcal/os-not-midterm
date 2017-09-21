4155ed7449e918c3ccecdd39ad1ba3024323c43a
komcal <kom.sujautra@gmail.com>
2017-09-21 21:09:41 +0700

    update input tid2

--- pthread.c
+++ pthread.c
@@ -7,9 +7,11 @@ void *runner(void *param); /*  threads call this function */
 
 int main(int argc, char *argv[])
 {
-  char num[100];
+  char num[100], num2[100];
   printf("input to tid1: ");
   scanf("%s", num);
+  printf("input to tid2: ");
+  scanf("%s", num2);
   pthread_t tid; /* the thread identifier */
   pthread_t tid2; /* the thread identifier */
   pthread_attr_t attr; /* set of thread attributes */
@@ -17,7 +19,7 @@ int main(int argc, char *argv[])
   pthread_attr_init(&attr);
   /* create the thread */
   pthread_create(&tid, &attr, runner, num); /* wait for the thread to exit */
-  pthread_create(&tid2, &attr, runner, "4"); /* wait for the thread to exit */
+  pthread_create(&tid2, &attr, runner, num2); /* wait for the thread to exit */
   pthread_join(tid,NULL);
   pthread_join(tid2,NULL);
   printf("sum = %d\n",sum);
