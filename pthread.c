eecaffc0ff393841c38d9407e0b3b4bc586e7730
komcal <kom.sujautra@gmail.com>
2017-09-21 22:46:19 +0700

    update thread to array

--- pthread.c
+++ pthread.c
@@ -1,27 +1,34 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#define NUM_THREDS 3
 
 int sum = 0; /*  this data is shared by the thread(s) */
 void *runner(void *param); /*  threads call this function */
 
 int main(int argc, char *argv[])
 {
-  char num[100], num2[100];
+  char num[3][100];
   printf("input to tid1: ");
-  scanf("%s", num);
+  scanf("%s", num[0]);
   printf("input to tid2: ");
-  scanf("%s", num2);
-  pthread_t tid; /* the thread identifier */
-  pthread_t tid2; /* the thread identifier */
+  scanf("%s", num[1]);
+  printf("input to tid3: ");
+  scanf("%s", num[2]);
+  
+  pthread_t tid[NUM_THREDS]; /* the thread identifier */
   pthread_attr_t attr; /* set of thread attributes */
 
   pthread_attr_init(&attr);
   /* create the thread */
-  pthread_create(&tid, &attr, runner, num); /* wait for the thread to exit */
-  pthread_create(&tid2, &attr, runner, num2); /* wait for the thread to exit */
-  pthread_join(tid,NULL);
-  pthread_join(tid2,NULL);
+  for(int i = 0 ; i < NUM_THREDS ; i++)
+  {
+    pthread_create(&tid[i], &attr, runner, num[i]);
+  }
+  for(int i = 0 ; i < NUM_THREDS ; i++)
+  {
+    pthread_join(tid[i],NULL);
+  }
   printf("sum = %d\n",sum);
 }
 /* The thread will begin control in this function */
